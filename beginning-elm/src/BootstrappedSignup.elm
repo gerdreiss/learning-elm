15b82b16fbeef12b0d95d643dd6e59e9b9127b52
Gerd Reiss <gerdreiss@jscriptive.com>
2019-11-16 14:21:08 +0100

    Forms and styling

---
+++ beginning-elm/src/BootstrappedSignup.elm
@@ -0,0 +1,91 @@
+module BootstrappedSignup exposing (User)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias User =
+    { name : String
+    , email : String
+    , password : String
+    , loggedIn : Bool
+    }
+
+type Msg
+    = SaveName String
+    | SaveEmail String
+    | SavePassword String
+    | Signup
+
+
+initialModel : User
+initialModel =
+    { name = ""
+    , email = ""
+    , password = ""
+    , loggedIn = False
+    }
+
+
+view : User -> Html msg
+view user =
+    div [ class "container" ]
+        [ div [ class "row" ]
+            [ div [ class "col-md-6 col-md-offset-3" ]
+                [ h1 [ class "text-center" ] [ text "Sign up" ]
+                , Html.form []
+                    [ div [ class "form-group" ]
+                        [ label
+                            [ class "control-label"
+                            , for "name"
+                            ]
+                            [ text "Name" ]
+                        , input
+                            [ class "form-control"
+                            , id "name"
+                            , type_ "text"
+                            ]
+                            []
+                        ]
+                    , div [ class "form-group" ]
+                        [ label
+                            [ class "control-label"
+                            , for "email"
+                            ]
+                            [ text "Email" ]
+                        , input
+                            [ class "form-control"
+                            , id "email"
+                            , type_ "email"
+                            ]
+                            []
+                        ]
+                    , div [ class "form-group" ]
+                        [ label
+                            [ class "control-label"
+                            , for "password"
+                            ]
+                            [ text "Password" ]
+                        , input
+                            [ class "form-control"
+                            , id "password"
+                            , type_ "password"
+                            ]
+                            []
+                        ]
+                    , div [ class "text-center" ]
+                        [ button
+                            [ class "btn btn-lg btn-primary"
+                            , type_ "submit"
+                            ]
+                            [ text "Create my account" ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+
+main : Html msg
+main =
+    view initialModel
---
+++ beginning-elm/src/SignupInlineStyle.elm
@@ -0,0 +1,84 @@
+module SignupInlineStyled exposing (User)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias User =
+    { name : String
+    , email : String
+    , password : String
+    , loggedIn : Bool
+    }
+
+
+initialModel : User
+initialModel =
+    { name = ""
+    , email = ""
+    , password = ""
+    , loggedIn = False
+    }
+
+
+view : User -> Html msg
+view user =
+    div []
+        [ h1 [ style "padding-left" "3cm" ] [ text "Sign up" ]
+        , Html.form formStyle
+            [ div []
+                [ text "Name"
+                , input ([ id "name", type_ "text", value user.name ] ++ inputStyle) []
+                ]
+            , div []
+                [ text "Email"
+                , input ([ id "email", type_ "email", value user.email ] ++ inputStyle) []
+                ]
+            , div []
+                [ text "Password"
+                , input ([ id "password", type_ "password" ] ++ inputStyle) []
+                ]
+            , div []
+                [ button ([ type_ "submit" ] ++ buttonStyle)
+                    [ text "Create my account" ]
+                ]
+            ]
+        ]
+
+
+formStyle : List (Attribute msg)
+formStyle =
+    [ style "border-radius" "5px"
+    , style "background-color" "#f2f2f2"
+    , style "padding" "20px"
+    , style "width" "300px"
+    ]
+
+
+inputStyle : List (Attribute msg)
+inputStyle =
+    [ style "display" "block"
+    , style "width" "260px"
+    , style "padding" "12px 20px"
+    , style "margin" "8px 0"
+    , style "border" "none"
+    , style "border-radius" "4px"
+    ]
+
+
+buttonStyle : List (Attribute msg)
+buttonStyle =
+    [ style "width" "300px"
+    , style "background-color" "#397cd5"
+    , style "color" "white"
+    , style "padding" "14px 20px"
+    , style "margin-top" "10px"
+    , style "border" "none"
+    , style "border-radius" "4px"
+    , style "font-size" "16px"
+    ]
+
+
+main : Html msg
+main =
+    view initialModel
